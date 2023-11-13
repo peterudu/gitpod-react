f39d47c9917a8b9c5bd6b7c1844b183246e54fd9
peterudu <puduevborho7@gmail.com>
2023-11-13 20:57:45 +0000

    Set up FunctionalGreetingWithProps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>It works</h1>
+      <FunctionalGreeting />
+      <FunctionalGreetingWithProps />
     </div>
   );
 }
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreetingWithProps() {
+//     return <h1>Hello</h1>
+// }
+
+const FunctionalGreetingWithProps = () => <h1>Hello!</h1>
+
+export default FunctionalGreetingWithProps;
