86bcf8d737dd44b0de803f65aede5f3b7782554a
peterudu <puduevborho7@gmail.com>
2023-11-17 21:52:08 +0000

    Pass methods as props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <FunctionalGreeting /> */}
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32"/>
-    </div>
-  );
+  return <div className="App">
+    <MethodsAsPropsParent />
+  </div>
 }
 
 export default App;
